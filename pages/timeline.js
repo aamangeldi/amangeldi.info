aaba5defb6e60c1a7c9024f84955da08fa444409
Amir Amangeldi <amir.amangeldi@gmail.com>
2020-03-03 13:43:35 -0800

    Add a few more facts

--- pages/timeline.js
+++ pages/timeline.js
@@ -5,15 +5,16 @@ export default function Resume() {
     '1996': ['Born in Almaty, Kazakhstan.'],
     '2000': ['Moved to Ashgabat, Turkmenistan.'],
     '2004': ['Moved to Kiev, Ukraine.'],
-    '2011': ['Moved to Oxford, UK, to study the IB program at St. Clare\'s College.'],
-    '2013': ['Moved to Middlebury, VT, to study at Middlebury College.'],
-    '2014': ['Took my first computer science course.'],
+    '2011': ['IB program at St. Clare\'s College in Oxford, UK.'],
+    '2012': ['Extended essay on Gulag labor camps.'],
+    '2013': ['Graduated from St. Clare\'s.','Flew to the US to study at Middlebury College in Vermont.'],
+    '2014': ['Took my first computer science class.'],
     '2016': ['Software development internship at InterSystems in Cambridge, MA.'],
     '2017': ['Built the Language Tables App.',
       'Graduated from Middlebury.',
       'Moved to Cambridge, MA to join InterSystems as a sales engineer.'
     ],
-    '2018': ['Joined EverQuote as a software engineer. Moved in with Elaine.'],
+    '2018': ['Joined EverQuote as a software engineer.'],
     '2019': ['Promoted to senior software engineer at EverQuote.', 'Adopted two kittens: Cece and Sasha.'],
   }
   const mapFacts = (facts) => {
