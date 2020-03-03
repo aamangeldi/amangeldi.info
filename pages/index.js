61a3baacdcb4baedc34bb755c0d2c1138a21a7f2
Amir Amangeldi <amir.amangeldi@gmail.com>
2020-03-03 12:15:28 -0800

    I do like building things !

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import Layout from '../components/layout';
 export default function Index() {
   return (
     <Layout>
-        <p>Hi! My name is Amir.<br/>I am a software engineer that works with data.</p>
+        <p>Hi! My name is Amir.</p>
+        <p>I am a software engineer that works with data and builds things.</p>
     </Layout>
   );
 }
