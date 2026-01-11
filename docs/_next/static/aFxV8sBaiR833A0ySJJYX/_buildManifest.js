b7b4456d7c70af610ac263c8e5274dd490bc2408
Amir Amangeldi <amir.amangeldi@gmail.com>
2026-01-11 17:45:30 -0500

    (Re)build and (re)deploy

---
+++ docs/_next/static/aFxV8sBaiR833A0ySJJYX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-653e9583f32dea5b.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
